513dd6c1f0aa3197ba0eb594eb19554568a8bc72
Ilamaran Gurunathan <ilamaran.g@gmail.com>
2017-08-21 11:34:31 -0400

    Added a basic framework for NYT Search project

---
+++ app/app.js
@@ -0,0 +1,13 @@
+// Include the Main React Dependencies
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+// Include the main Main Components
+
+
+// This code here allows us to render our main component (in this case Main)
+ReactDOM.render(
+<div>
+  <h1> Hello </h1>
+
+</div>, document.getElementById("app"));
---
+++ server.js
@@ -0,0 +1,69 @@
+// Include Server Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+
+
+// Create Instance of Express
+var app = express();
+// Sets an initial port. We'll use this later in our listener
+var PORT = process.env.PORT || 3000;
+
+
+if (process.env.NODE_ENV === 'production') {
+var MONGODB = "mongodb://heroku_76l9r11f:kio2gu6in6vi2l0san2ck8ib02@ds159180.mlab.com:59180/heroku_76l9r11f";
+}
+else {
+var MONGODB = "mongodb://localhost/hwNYTScrape";
+}
+
+// Run Morgan for Logging
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+app.use(express.static("public"));
+
+// -------------------------------------------------
+
+// MongoDB Configuration configuration (Change this URL to your own DB)
+mongoose.connect("mongodb://admin:codingrocks@ds023674.mlab.com:23674/heroku_5ql1blnl");
+var db = mongoose.connection;
+
+db.on("error", function(err) {
+  console.log("Mongoose Error: ", err);
+});
+
+db.once("open", function() {
+  console.log("Mongoose connection successful.");
+});
+
+// -------------------------------------------------
+
+// Main "/" Route. This will redirect the user to our rendered React application
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+// This is the route we will send GET requests to retrieve our most recent search data.
+// We will call this route the moment our page gets rendered
+app.get("/api", function(req, res) {
+
+
+});
+
+// This is the route we will send POST requests to save each search.
+app.post("/api", function(req, res) {
+
+});
+
+// -------------------------------------------------
+
+// Listener
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
