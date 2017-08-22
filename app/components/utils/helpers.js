20b09d8010c67cddeefe247ab7c8b59a52a3bfd9
Ilamaran Gurunathan <ilamaran.g@gmail.com>
2017-08-22 00:08:10 -0400

    Integrated Date Parameters with Search

--- app/components/utils/helpers.js
+++ app/components/utils/helpers.js
@@ -1,5 +1,6 @@
 // Include the axios package for performing HTTP requests (promise based alternative to request)
 import axios from "axios";
+import moment from "moment";
 
 // Geocoder API
 const APIKey = "9e38d3001eee43ba8a1d2a51ab80a59a";
@@ -10,14 +11,19 @@ const helpers = {
   runQuery: (searchParams) => {
 
     console.log(location);
+    var fromDate = moment(searchParams.fromDate).format("YYYYMMDD");
 
+     var toDate = moment(searchParams.toDate).format("YYYYMMDD");
     // Figure out the geolocation
     const queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    console.log(moment(searchParams.fromDate).format("YYYY/MM/DD"));
 
     return axios.get(queryURL,{
       params :{
         'api-key': APIKey,
-        'q': searchParams.searchTerm
+        'q': searchParams.searchTerm,
+        begin_date: fromDate,
+        end_date : toDate
       }
     }).then((res) => {
 
